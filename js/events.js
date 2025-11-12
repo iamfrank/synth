41e044970a9a2de4e8d59d97ce535e04527b3d05
Frank R Thomsen <frank@septima.dk>
2025-11-12 21:32:52 +0100

    made a tiny synth

---
+++ js/events.js
@@ -0,0 +1,61 @@
+import { startTone, endTone } from "./synth.js";
+
+function keyToneMapper(keycode) {
+  switch (keycode) {
+    case "KeyA":
+      return 220;
+    case "KeyS":
+      return 246.9;
+    case "KeyD":
+      return 261.6;
+    case "KeyF":
+      return 293.7;
+    case "KeyG":
+      return 329.6;
+    case "KeyH":
+      return 349.2;
+    case "KeyJ":
+      return 392;
+    case "KeyK":
+      return 440;
+    default:
+      return false;
+  }
+}
+
+function keydownHandler(event) {
+  const freq = keyToneMapper(event.code);
+  if (freq) {
+    startTone(event.code, freq);
+    console.log("hit button", event.code);
+    document
+      .querySelector(`[data-keycode="${event.code}"]`)
+      .classList.add("active");
+  }
+}
+
+function keyupHandler(event) {
+  const freq = keyToneMapper(event.code);
+  if (freq) {
+    endTone(event.code);
+    document
+      .querySelector(`[data-keycode="${event.code}"]`)
+      .classList.remove("active");
+  }
+}
+
+function pointerdownHandler(event) {
+  const freq = keyToneMapper(event.target.dataset.keycode);
+  if (freq) {
+    startTone(event.target.dataset.keycode, freq);
+  }
+}
+
+function pointerupHandler(event) {
+  const freq = keyToneMapper(event.target.dataset.keycode);
+  if (freq) {
+    endTone(event.target.dataset.keycode);
+  }
+}
+
+export { keydownHandler, keyupHandler, pointerdownHandler, pointerupHandler };
---
+++ js/synth.js
@@ -0,0 +1,38 @@
+const audioCtx = new AudioContext();
+const keyMap = new Map();
+
+function startTone(keycode, freq) {
+  const osc = keyMap.get(keycode);
+  if (!osc) {
+    keyMap.set(keycode, playSound(freq));
+  }
+}
+
+function endTone(keycode) {
+  const { osc, gain } = keyMap.get(keycode);
+  if (osc) {
+    gain.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 1); // Release
+    osc.stop(audioCtx.currentTime + 1);
+  }
+  keyMap.delete(keycode);
+}
+
+function playSound(freq) {
+  if (!freq) {
+    return;
+  }
+  const osc = audioCtx.createOscillator();
+  const gain = audioCtx.createGain();
+
+  osc.type = "square";
+  osc.frequency.value = freq;
+
+  gain.gain.setValueAtTime(0, audioCtx.currentTime);
+  gain.gain.linearRampToValueAtTime(1, audioCtx.currentTime + 0.1); // Attack
+
+  osc.connect(gain).connect(audioCtx.destination);
+  osc.start();
+  return { osc, gain };
+}
+
+export { startTone, endTone };
