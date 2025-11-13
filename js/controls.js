fe2faf4d8b4af90ac92dab4dfbc27e1527cea948
Frank R Thomsen <frank@septima.dk>
2025-11-13 09:23:47 +0100

    fix type

--- js/controls.js
+++ js/controls.js
@@ -3,23 +3,23 @@ function getOscType() {
 }
 
 function getVolume() {
-  return document.getElementById("vol").value;
+  return Number(document.getElementById("vol").value);
 }
 
 function getAttack() {
-  return document.getElementById("attack").value;
+  return Number(document.getElementById("attack").value);
 }
 
 function getDecay() {
-  return document.getElementById("decay").value;
+  return Number(document.getElementById("decay").value);
 }
 
 function getSustain() {
-  return document.getElementById("sustain").value;
+  return Number(document.getElementById("sustain").value);
 }
 
 function getRelease() {
-  return document.getElementById("release").value;
+  return Number(document.getElementById("release").value);
 }
 
 export { getOscType, getVolume, getAttack, getDecay, getSustain, getRelease };
